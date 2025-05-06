318d38fcdd0f24047900b20a9fc3418ef3103d58
CWXxx <xianxian599@gmail.com>
2025-05-06 17:28:21 +0800

    header updated

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-    $("#colorButton").click(function() {
-        $("#colorAccessibility").fadeToggle(300);
-        $("#fontSizeAccessibility").fadeOut(300);
-        $("#support").fadeOut(300);
+    $(".colorButton").click(function() {
+        $(".colorAccessibility").fadeToggle(300);
+        $(".fontSizeAccessibility").fadeOut(300);
+        $(".support").fadeOut(300);
     });
 
-    $("#fontSizeButton").click(function() {
-        $("#colorAccessibility").fadeOut(300);
-        $("#fontSizeAccessibility").fadeToggle(300);
-        $("#support").fadeOut(300);
+    $(".fontSizeButton").click(function() {
+        $(".colorAccessibility").fadeOut(300);
+        $(".fontSizeAccessibility").fadeToggle(300);
+        $(".support").fadeOut(300);
     });
 
-    $("#supportButton").click(function() {
-        $("#colorAccessibility").fadeOut(300);
-        $("#fontSizeAccessibility").fadeOut(300);
-        $("#support").fadeToggle(300);
+    $(".supportButton").click(function() {
+        $(".colorAccessibility").fadeOut(300);
+        $(".fontSizeAccessibility").fadeOut(300);
+        $(".support").fadeToggle(300);
     });
 
     $("#burgerIcon").click(function() {

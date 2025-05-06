be3fd131e6025bda87badb09f4aa90f9cdf1139a
CWXxx <xianxian599@gmail.com>
2025-05-06 16:37:39 +0800

    header footer updated

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $("#colorButton").click(function() {
+        $("#colorAccessibility").fadeToggle(300);
+        $("#fontSizeAccessibility").fadeOut(300);
+        $("#support").fadeOut(300);
+    });
+
+    $("#fontSizeButton").click(function() {
+        $("#colorAccessibility").fadeOut(300);
+        $("#fontSizeAccessibility").fadeToggle(300);
+        $("#support").fadeOut(300);
+    });
+
+    $("#supportButton").click(function() {
+        $("#colorAccessibility").fadeOut(300);
+        $("#fontSizeAccessibility").fadeOut(300);
+        $("#support").fadeToggle(300);
+    });
+
+    $("#burgerIcon").click(function() {
+        if ($("aside").hasClass("visible")) {
+            $("aside").fadeOut(300, function() {
+                $("aside").removeClass("visible");
+            });
+        } else {
+            $("aside").addClass("visible").hide().fadeIn(300);
+        }
+    });
+});

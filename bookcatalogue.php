f0ea65aa1ad015fb570daa04a902241d366c5e85
milo <anthanielaniel05@gmail.com>
2025-06-24 18:43:21 +0800

    Update bookcatalogue.php

--- bookcatalogue.php
+++ bookcatalogue.php
@@ -40,7 +40,6 @@ $comments = [
     ['user' => 'XXX2', 'comment' => 'In my opinion, I think yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy.'],
 ];
 
-
 ?>
 <!DOCTYPE html>
 <html lang="en" data-themeColor="defaultColor" data-fontSize="defaultFontSize">
@@ -48,13 +47,15 @@ $comments = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BookSpare - Book Detail</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico"> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     <style>
         :root {
             --containerBgColor: #f5f5f5;
             --containerColor: black;
             --containerBoxShadow: 1px 1px 10px 5px rgba(0, 0, 0, 0.225);
             --contentBgColor: white;
-            --borderColor: black; /* Used for borders that should change with theme */
+            --borderColor: black;
 
             --buttonColor: #a9a1ee;
             --buttonFontColor: black;
@@ -137,71 +138,6 @@ $comments = [
             --commentsBoxBg: #333;
             --footerBgColor: #3a3a3a;
             --avatarBgColor: #666;
-        }:root {
-            --containerBgColor: #f5f5f5;
-            --containerColor: black;
-            --containerBoxShadow: 1px 1px 10px 5px rgba(0, 0, 0, 0.225);
-            --contentBgColor: white;
-            --borderColor: black;
-
-            --buttonColor: #a9a1ee;
-            --buttonFontColor: black;
-            --buttonHoverColor: #d8d5ec;
-
-            --postHeaderBgColor: rgb(220, 196, 238);
-            --postBgColor: white;
-
-            --commentButtonColor: rgb(161, 178, 238);
-            --commentButtonFontColor: black;
-            --commentButtonFontColorActive: black;
-            --commentButtonHoverColor: rgb(205, 212, 234);
-
-            --linkColor: blue;
-        }
-
-        [data-themeColor="lightColor"] {
-            --containerBgColor: #f5f5f5;
-            --containerColor: black;
-            --containerBoxShadow: 1px 1px 10px 5px rgba(0, 0, 0, 0.225);
-            --contentBgColor: white;
-            --borderColor: black;
-
-            --buttonColor: black;
-            --buttonFontColor: white;
-            --buttonHoverColor: #646368;
-
-            --postHeaderBgColor: white;
-            --postBgColor: white;
-
-            --commentButtonColor: rgb(23, 24, 25);
-            --commentButtonFontColor: white;
-            --commentButtonFontColorActive: rgb(134, 155, 195);
-            --commentButtonHoverColor: rgb(91, 87, 87);
-
-            --linkColor: blue;
-        }
-
-        [data-themeColor="darkColor"] {
-            --containerBgColor: rgb(34, 34, 34);
-            --containerColor: rgb(213, 213, 213);
-            --containerBoxShadow: 1px 1px 20px 1px rgba(255, 255, 255, 0.822);
-            --contentBgColor: rgb(53, 53, 53);
-            --borderColor: white;
-
-            --buttonColor: black;
-            --buttonFontColor: white;
-            --buttonHoverColor: #8d8c8c;
-
-            
-            --postHeaderBgColor: rgb(1, 1, 1);
-            --postBgColor: rgb(45, 45, 45);
-
-            --commentButtonColor: rgb(23, 24, 25);
-            --commentButtonFontColor: white;
-            --commentButtonFontColorActive: white;
-            --commentButtonHoverColor: rgb(91, 87, 87);
-
-            --linkColor: rgb(119, 167, 190);
         }
 
         body {
@@ -246,7 +182,7 @@ $comments = [
             border-bottom: 2px solid var(--borderColor);
         }
 
-        .tabs span[style*="color: gray"] {
+        .tabs span.inactive-tab {
             border-bottom: none;
             font-weight: normal;
             color: gray;
@@ -391,31 +327,10 @@ $comments = [
     </style>
 </head>
 <body>
-    <header>
-        <div class="logo">📖 BookSpare</div>
-        <div class="menu">
-            <div>Colour ▼</div>
-            <div>Font Size ▼</div>
-            <div>Support ▼</div>
-            <div>🔔</div>
-            <div><?php echo htmlspecialchars($user['username'] ?? 'User'); ?></div>
-            <div class="avatar" style="background-color: var(--avatarBgColor, var(--avatarBgColorDefault));">
-                <?php echo htmlspecialchars(substr($user['username'] ?? 'U', 0, 1)); ?>
-            </div>
-        </div>
-    </header>
-    <div class="tab-bar">
-        <div class="tabs">
-            <span>Main</span>
-            <span style="color: gray; font-weight: normal; border: none;">Genre</span>
-        </div>
-        <div class="search">
-            🔍 <input type="text" placeholder="Search">
-        </div>
-        <div>Logout</div>
-    </div>
+    <?php include("header.php"); ?>
+
     <main>
-        <div class="back-button">⬅ Back</div>
+        <div class="back-button" onclick="history.back()">⬅ Back</div>
         <h2>Detail of the Book</h2>
         <div class="book-details">
             <?php if ($bookDetails && $bookDetails['front_cover_path']): ?>
@@ -454,11 +369,7 @@ $comments = [
             </div>
         </div>
     </main>
-    <div class="footer">
-        <p>Follow Us At</p>
-        <p>📷</p>
-        <p>Copyright © 2025 BookSpare. All right reserved</p>
-    </div>
+    <?php include("footer.html"); ?>
 </body>
 </html>
 <?php
